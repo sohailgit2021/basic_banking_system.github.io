5bc2862580cbb548347feab7d986a52b5de4cbe1
sohailgit2021 <khansohail08355@gmail.com>
2022-03-15 15:56:00 +0530

    this is spark foundation basic banking system

---
+++ footer.php
@@ -0,0 +1,65 @@
+<footer class="bg-dark text-center pt-5 pb-4">
+        <div class="container text-md-left">
+            <div class="row text-center text-md-left">
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Services</h5>
+
+                    <p class="nav-item"><a href="" class=""
+                            style="text-decoration: none ; color: white; font-weight: inherit;">Send Money</a></p>
+                    <p class="nav-item"><a href="" class=""
+                            style="text-decoration:none ; color: white; font-weight: inherit;">View All Customers</a></p>
+                    <p class="nav-item "><a href="" class=""
+                            style="text-decoration:none ; color: white;font-weight: inherit;">Transactions</a></p>
+                </div>
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-warning">useful links</h5>
+                    <p class="nav-item"><a href="" class=""
+                            style="text-decoration: none ; color: white; font-weight: inherit;">Home</a></p>
+                    <p class="nav-item"><a href="" class=""
+                            style="text-decoration:none ; color: white; font-weight: inherit;">Services</a></p>
+                    <p class="nav-item "><a href="" class=""
+                            style="text-decoration:none ; color: white;font-weight: inherit;">Transactions</a></p>
+                            <p class="nav-item "><a href="" class=""
+                            style="text-decoration:none ; color: white;font-weight: inherit;">Contact Us</a></p>
+                    <p class="nav-item "><a href="" class=""
+                            style="text-decoration:none ; color: white;font-weight: inherit; ">About Us</a></p>
+                </div>
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-warning">contact</h5>
+                    <p class="text-white"><i class="fas fa-home mr-3">&nbsp;&nbsp;21 Heng Mui Keng Terrace, Icube Building, Singapore 119613</i></p>
+                    <p class="text-white"><i class="fas fa-envelope mr-3">&nbsp;&nbsp;info@thesparksfoundation.sg</i></p>
+                    <p class="text-white"><i class="fas fa-phone mr-3">&nbsp;&nbsp; Call Us on  +65 8402 8590</i></p>
+                </div>
+            </div>
+            <hr class="mb-4">
+            <div class="row align-item-center">
+                <div class="col-md-7 col-lg-8">
+                    <p style="color: white">All &#9400;Copyright are reserved. This website is designed by: <a href=""
+                            style="text-decoration: none;"> <strong class="text-warning font-weight-bold">@ Md Sohail
+                                Khan</strong></a></p>
+                </div>
+                <div class="col-md-5 col-lg-4">
+                    <div class="text-center text-md-right">
+                        <ul class="list-unstyled list-inline">
+                            <li class="list-inline-item">
+                                <a href="https://www.facebook.com/thesparksfoundation.info" class="btn-floating btn-sm text-warning" style="font-size: 23px;"><i
+                                        class="fab fa-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://www.instagram.com/thesparksfoundation.info/" class="btn-floating btn-sm text-warning" style="font-size: 23px;"><i
+                                        class="fab fa-instagram"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://www.linkedin.com/company/the-sparks-foundation/" class="btn-floating btn-sm text-warning" style="font-size: 23px;"><i
+                                        class="fab fa-linkedin"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://medium.com/thesparksfoundation" class="btn-floating btn-sm text-warning" style="font-size: 23px;"><i
+                                        class="fab fa-google-plus"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
